8f9839daa77b282dacadcfed2d4bc74b7a525cf0
Pateel H S <pateelhs@gmail.com>
2023-10-03 23:30:22 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-053b0d53c279acc90"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0ccd55ff1af35d630"
+    key_name = "test_terra"
 }
